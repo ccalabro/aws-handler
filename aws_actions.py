917e9076cbfdf24b5b9a701b991bf031fa42edcd
Cristian Calabro <cristian.calabro@gmail.com>
2014-03-29 20:11:40 -0300

    Initial commit.

---
+++ aws_actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Cristian Calabro"
+__credits__ = ["Cristian Calabro"]
+__version__ = "0.1"
+__maintainer__ = "Cristian Calabro"
+__email__ = "cristian.calabro@gmail.com"
+__status__ = "Development"
+
+if __name__ == '__main__':
+	pass
